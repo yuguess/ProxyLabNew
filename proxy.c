cdc3e7055edfa7e5f1fd9f66abd3b88df3f7eb8c
Dalong Cheng <yuguess@gmail.com>
2011-11-28 14:42:25 -0500

    change some code to prepare for cache implementation

--- proxy.c
+++ proxy.c
@@ -7,26 +7,53 @@
 #include <string.h>
 #include "csapp.h"
 
+#define MAX_OBJECT_SIZE ((1 << 10) * 100) 
+#define MAX_CACHE_SIZE (1 << 20)
+
+int min(int a, int b) {
+    return a > b ? b : a;
+}
+
+typedef struct Cache_Block Cache_Block;
+
+struct Cache_Block{
+    unsigned long key;
+    char *content;
+    size_t content_size;
+    time_t time_stamp;
+    Cache_Block *next_block;
+};
+
+typedef struct {
+    size_t size; 
+    Cache_Block *start; 
+} Cache;
+
 typedef struct {
     char request_str[MAXLINE]; 
     char host_str[MAXLINE];
 } Request;
-/*
-typedef struct {
-    char 
-} Response_Header;
-*/
 
 typedef struct {
     char header[MAXLINE];
+    char *content;
+    int content_length;
 } Response;
 
 typedef struct {
     int client_fd;
 } Thread_Input;
 
+Cache cache;
+
+void proxy_error(char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+}
+
 static inline void copy_single_line_str(rio_t *client_rio, char *buffer) {
-    rio_readlineb(client_rio, buffer, MAXLINE);
+    if (rio_readlineb(client_rio, buffer, MAXLINE) < 0) {
+        proxy_error("copy_single_lienstr error");
+    }
     buffer[strlen(buffer)] = '\0';
 }
 
@@ -35,6 +62,110 @@ static inline void extract_hostname(char *src, char *desc) {
     copy_length = strlen(src + 6) - 2;  
     strncpy(desc, src + 6, copy_length); 
     desc[copy_length] = '\0';
+}
+
+void modify_request_header(Request *request) {
+    char *str;
+    if ((str = strstr(request->request_str, "HTTP/1.1")) != NULL) {
+        strncpy(str, "HTTP/1.0", 8); 
+    } else {
+        fprintf(stderr, "Warning: HTTP/1.1 not found in request\n");
+    }
+    #ifdef DEBUG
+    printf("modify request str:%s", request->request_str);
+    #endif
+}
+
+void send_client(Response *response) {
+
+}
+
+int check_cache(Request *request, Response *response) {
+    return 0;
+}
+
+static inline int extract_port_number(char *resquest_str) {
+    return 80;
+}
+
+void forward_response(int client_fd, rio_t *server_rio, Response *response) {
+    size_t n;
+    int length = -1;
+    char header_buffer[MAXLINE];
+    char content_buffer[MAX_OBJECT_SIZE];
+    int read_size;
+    
+    while ((n = rio_readlineb(server_rio, header_buffer, MAXLINE)) != 0) { 
+        strcat(response->header, header_buffer); 
+        /*
+        if (rio_writen(client_fd, header_buffer, n) < 0) {
+            proxy_error("rio_writen in forward_response header error");  
+        }*/
+        
+        if (strstr(header_buffer, "Content-Length: ")) {
+            sscanf(header_buffer + 16, "%d", &length);
+        }
+        if (!strcmp(header_buffer, "\r\n")) {
+            break;
+        }
+    }
+
+    if (length == -1)
+        read_size = MAX_OBJECT_SIZE;
+    else 
+        read_size = min(length, MAX_OBJECT_SIZE);
+    
+    #ifdef DEBUG
+    printf("response header: %s", response->header);
+    #endif
+
+    int sum = 0;
+    while ((n = rio_readnb(server_rio, content_buffer, read_size)) != 0) { 
+        if (rio_writen(client_fd, content_buffer, n) < 0) {
+            proxy_error("rio_writen in forward_response content error");  
+            Close(client_fd);
+        }
+        sum += n;
+    }
+
+    #ifdef DEBUG 
+    printf("read byte size:%u\n", sum);
+    #endif
+   /* 
+    if (sum <= MAX_OBJECT_SIZE) {
+        response->content = Malloc(sizeof(char) * sum);
+        response->content_length = sum;
+    } else {
+        response->content_length = sum;
+    }*/
+}
+
+void forward_request(int client_fd, Request *request, Response *response) {
+    rio_t   server_rio;
+    int server_fd;
+    char hostname[MAXLINE];
+    int  port = 80;
+
+    port = extract_port_number(request->request_str);
+    extract_hostname(request->host_str, hostname);
+
+    #ifdef DEBUG
+    printf("hostname:%s\n", hostname);
+    printf("port:%d\n", port);
+    #endif
+
+    server_fd = open_clientfd(hostname, port);
+
+    rio_readinitb(&server_rio, server_fd);
+    #ifdef DEBUG
+    printf("request_str:%s", request->request_str); 
+    #endif
+    
+    rio_writen(server_fd, request->request_str, strlen(request->request_str));
+    rio_writen(server_fd, request->host_str, strlen(request->host_str));
+    rio_writen(server_fd, "\r\n", strlen("\r\n"));
+    
+    forward_response(client_fd, &server_rio, response);
 }
 
 void parse_request_header(int client_fd, Request *request) {
@@ -61,87 +192,19 @@ void parse_request_header(int client_fd, Request *request) {
     }
 }
 
-void modify_request_header(Request *request) {
-    char *str;
-    if ((str = strstr(request->request_str, "HTTP/1.1")) != NULL) {
-        strncpy(str, "HTTP/1.0", 8); 
-    }
-    #ifdef DEBUG
-    printf("modify request str:%s", request->request_str);
-    #endif
-}
+void save_to_cache(Response *response) {
+    //lock !
+    /*
+    if (cache) {
 
-void send_client(Response *response) {
+    }*/
 
-}
-
-int check_cache(Request *request, Response *response) {
-    return 0;
-}
-
-static inline int extract_port_number(char *resquest_str) {
-    return 80;
-}
-
-void forward_response(int client_fd, rio_t *server_rio, Response *response) {
-    size_t n;
-    int length;
-    char buffer[MAXLINE];
-    char *content;
-
-    while ((n = rio_readlineb(server_rio, buffer, MAXLINE)) != 0) { 
-        strcat(response->header, buffer); 
-        rio_writen(client_fd, buffer, n);
-        if (strstr(buffer, "Content-Length: ")) {
-            sscanf(buffer + 16, "%d", &length);
-            //sscanf(buffer, "%d", &length);
-            printf("length:%d\n", length);
-        }
-        if (!strcmp(buffer, "\r\n")) {
-            break;
-        }
-    }
-    #ifdef DEBUG
-    printf("response header: %s", response->header);
-    #endif
-    
-    content = (char*)malloc(sizeof(char) * length);
-    while ((n = rio_readnb(server_rio, content, length)) != 0) { 
-        rio_writen(client_fd, content, n);
-    }
-    free(content);
-}
-
-void forward_request(int client_fd, Request *request, Response *response) {
-    rio_t   server_rio;
-    int server_fd;
-    char hostname[MAXLINE];
-    int  port = 80;
-
-    port = extract_port_number(request->request_str);
-    extract_hostname(request->host_str, hostname);
-
-    #ifdef DEBUG
-    printf("hostname:%s\n", hostname);
-    printf("port:%d\n", port);
-    #endif
-
-    server_fd = Open_clientfd(hostname, port);
-
-    rio_readinitb(&server_rio, server_fd);
-    #ifdef DEBUG
-    printf("request_str:%s", request->request_str); 
-    #endif
-    
-    rio_writen(server_fd, request->request_str, strlen(request->request_str));
-    rio_writen(server_fd, request->host_str, strlen(request->host_str));
-    rio_writen(server_fd, "\r\n", strlen("\r\n"));
-    
-    forward_response(client_fd, &server_rio, response);
+    //unlock !
 }
 
 void *request_handler(void *ptr) {
     int client_fd = ((Thread_Input*)ptr)->client_fd; 
+    printf("client_fd:%d\n", client_fd);
     Request request;
     Response response;
     parse_request_header(client_fd, &request);
@@ -151,7 +214,10 @@ void *request_handler(void *ptr) {
         send_client(&response);
     } else {
         forward_request(client_fd, &request, &response);
-        //send_client(client_fd, &response);
+        /*
+        if (response.length <= MAX_OBJECT_SIZE)
+            save_to_cache(&response);
+            */
     }
     free(ptr);
     Close(client_fd);
@@ -171,23 +237,34 @@ void scheduler(int client_fd) {
     Pthread_detach(tid);
 }
 
+void init_cache() {
+    cache.start = NULL;
+    cache.size = 0;
+}
+
+void clean_cache() {
+
+}
+
 int main (int argc, char *argv []) {
     int listen_fd, port;
     int client_fd;
     socklen_t socket_length;
     struct sockaddr_in client_socket;
 
+    init_cache();
     if (argc != 2) {
 	    fprintf(stderr, "usage: %s <port>\n", argv[0]);
 	    exit(0);
     }
 
     port = atoi(argv[1]);
-    listen_fd = Open_listenfd(port);
+    listen_fd = open_listenfd(port);
     socket_length = sizeof(client_socket);
 
     while (1) {
         client_fd = Accept(listen_fd, (SA*)&client_socket, &socket_length);   
         scheduler(client_fd);
     }
+    clean_cache();
 }
